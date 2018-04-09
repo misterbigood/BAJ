fc24e4ffaf02e2189908992c828c44fcb333c57f
C. Violeau (MDF) <c.violeau@mdfconseil.fr>
2018-04-09 22:57:32 +0200

    Merge CSS-aj
    Conflicts:
    	css/style.css.map
    	template-parts/content-visite.php

--- template-parts/content-visite.php
+++ template-parts/content-visite.php
@@ -93,7 +93,7 @@
             the_post_navigation( array (
                 'in_same_term'               => true,
             ));
-            the_category();
+            
             // If comments are open or we have at least one comment, load up the comment template.
             if (comments_open() || get_comments_number()) :
                 comments_template();
